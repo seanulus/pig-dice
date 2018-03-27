0644d874190cdfaad058c026554b1a1415c01aac
Jake Soffer and Sean McDermott <jake@>
2018-03-26 20:34:09 -0700

    Cry.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
+function PlayerScore(currentScore, totalScore) {
+  this.currentScore = currentScore;
+  this.totalScore = totalScore;
+}
+
+PlayerScore.prototype.addToCurrent = function() {
+  this.currentScore += this.diceRoll;
+}
+
+
+
 $(document).ready(function() {
-  $("#roll").click(function() {
+  var player1 = new PlayerScore();
+  var player2 = new PlayerScore();
+  $("#roll,#roll1").click(function() {
     var diceRoll = Math.floor(Math.random() * 6) + 1;
-    console.log(diceRoll);
-    $(".diceNumber").text(diceRoll);
+    addToCurrent(diceRoll);
+
+
     if (diceRoll === 1) {
       $("#roll").hide();
     }
-
+  });
+  $("#hold,#hold1").click(function() {
+    sumArray.length = 0;
+    $("#roll").show();
   });
 });
