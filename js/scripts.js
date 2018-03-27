8f921c09d90797a0f9e05c52f29a3519475e047a
Jake Soffer and Sean McDermott <jake@>
2018-03-26 18:53:30 -0700

    add if statement to hide if 1 is rolled

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
   $("#roll").click(function() {
     var diceRoll = Math.floor(Math.random() * 6) + 1;
     console.log(diceRoll);
+    $(".diceNumber").text(diceRoll);
+    if (diceRoll === 1) {
+      $("#roll").hide();
+    }
+
   });
 });
